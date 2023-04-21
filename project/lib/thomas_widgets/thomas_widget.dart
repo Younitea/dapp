dd095d6376b44e35bb492429b4bffcec37fde8d8
Younitea <thomasrchiesa@gmail.com>
2023-04-21 14:45:28 -0700

    commit to pull

--- project/lib/thomas_widgets/thomas_widget.dart
+++ project/lib/thomas_widgets/thomas_widget.dart
@@ -15,103 +15,41 @@ class ThomasWidget extends ConsumerWidget {
         child: ListView.builder(
             itemCount: skillList.length,
             itemBuilder: (context, index) {
-              return GestureDetector(
-                onTap: () {
-                  print("tapped on");
-                },
-                child: Container(
-                    alignment: Alignment.center,
-                    height: 40,
-                    decoration: const BoxDecoration(),
-                    child: Column(
-                      children: [
-                        Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                skillList[index].skillName,
-                                textAlign: TextAlign.left,
-                              ),
-                              GestureDetector(
-                                onTap: () {
-                                  rollSkill(
-                                      "",
-                                      skillList[index].prof,
-                                      skillList[index].expert,
-                                      skillList[index].stat);
-                                },
-                                child: const Text("placeholder"),
-                              )
-                            ])
-                      ],
-                    )),
-              );
+              return Button(skillList[index]);
             }));
   }
 }
 
-class SkillWidget extends StateNotifier<List<Skill>> {
-  SkillWidget() : super(const []);
-  static final provider =
-      StateNotifierProvider<SkillWidget, List<Skill>>((ref) {
-    return SkillWidget();
-  });
+class Button extends ConsumerWidget{
+  const Button(this.skill, {super.key});
 
-  void setProf(Skill skill) {}
-}
-/*
-class ThomasWidget extends ConsumerWidget {
-  const ThomasWidget({super.key});
-  
-  static final Provider = StateNotifierProvider<ThomasWidget, List<Skill>>((ref))
-  int _rollSkill(String skill, bool prof, bool exp, int stat) {
-    //todo, ouput skill
-    return 0;
-  }
+  final Skill skill;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) => Column(
-    List<Skill> skillList = ref.watch(skillProvider);
-        children: [
-          SizedBox(
-              height: 400,
-              width: 400,
-              child: ListView.builder(
-                  itemCount: skillList.length,
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                      onTap: () {
-                        print("tapped on");
-                      },
-                      child: Container(
-                          alignment: Alignment.center,
-                          height: 40,
-                          decoration: BoxDecoration(),
-                          child: Column(
-                            children: [
-                              Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Text(
-                                      skillList[index].skillName,
-                                      textAlign: TextAlign.left,
-                                    ),
-                                    GestureDetector(
-                                      onTap: () {
-                                        _rollSkill(
-                                            skillList[index].skillName,
-                                            skillList[index].prof,
-                                            skillList[index].expert,
-                                            skillList[index].stat);
-                                      },
-                                      child: Text("placeholder"),
-                                    )
-                                  ])
-                            ],
-                          )),
-                    );
-                  })),
-        ],
-      );
-}*/
+  Widget build(BuildContext context, WidgetRef ref){
+    return ListTile(
+      tileColor: Colors.orange, 
+      trailing: ElevatedButton(
+        style: ElevatedButton.styleFrom(backgroundColor: Colors.lightBlue),
+        onPressed: (){
+          ref.read(skillProvider.notifier).setProf(skill);
+        },
+        child: const Text('Swap Prof'),
+      ),
+    );
+  }
+}
+
+class SkillNotifier extends StateNotifier<List<Skill>> {
+  SkillNotifier() : super(const []);
+  void setProf(Skill skill) {
+    state.map((skill){
+      return Skill(skill.skillName, !skill.prof, skill.expert, skill.stat)
+    })  .toList();
+  }
+}
+
+ final provider =
+      StateNotifierProvider<SkillNotifier, List<Skill>>((ref) {
+    return SkillNotifier();
+  });
