c5f3e253aeb73a9a19558c0657129d1574db21c1
Younitea <thomasrchiesa@gmail.com>
2023-04-24 16:31:21 -0700

    skills are working

--- project/lib/thomas_widgets/thomas_widget.dart
+++ project/lib/thomas_widgets/thomas_widget.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
-import '../shared/roll_skill.dart';
-import '../shared/providers.dart';
 import 'skill.dart';
 
 class ThomasWidget extends ConsumerWidget {
@@ -14,6 +12,7 @@ class ThomasWidget extends ConsumerWidget {
         width: 400,
         child: ListView.builder(
             itemCount: skillList.length,
+            shrinkWrap: true,
             itemBuilder: (context, index) {
               return Button(skillList[index]);
             }));
@@ -28,7 +27,9 @@ class Button extends ThomasWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return ListTile(
-      tileColor: Colors.orange,
+      leading:
+          Icon(skill.prof ? Icons.check_box : Icons.check_box_outline_blank),
+      title: Text(skill.skillName),
       trailing: ElevatedButton(
         style: ElevatedButton.styleFrom(backgroundColor: Colors.lightBlue),
         onPressed: () {
@@ -41,10 +42,33 @@ class Button extends ThomasWidget {
 }
 
 class SkillNotifier extends StateNotifier<List<Skill>> {
-  SkillNotifier() : super(const []);
+  SkillNotifier()
+      : super(<Skill>[
+          Skill("Acrobatics", false, false, 0),
+          Skill("Animal handling", false, false, 0),
+          Skill("Arcana", false, false, 0),
+          Skill("Athletics", false, false, 0),
+          Skill("Deception", false, false, 0),
+          Skill("History", false, false, 0),
+          Skill("Insight", false, false, 0),
+          Skill("Intimidation", false, false, 0),
+          Skill("Investigation", false, false, 0),
+          Skill("Medicine", false, false, 0),
+          Skill("Nature", false, false, 0),
+          Skill("Perception", false, false, 0),
+          Skill("Performance", false, false, 0),
+          Skill("Persuasion", false, false, 0),
+          Skill("Religon", false, false, 0),
+          Skill("Sleight of Hand", false, false, 0),
+          Skill("Stealth", false, false, 0),
+          Skill("Survival", false, false, 0),
+        ]);
   void setProf(Skill skill) {
-    state.map((skill) {
-      return Skill(skill.skillName, !skill.prof, skill.expert, skill.stat);
+    final name = skill.skillName;
+    state = state.map((skill) {
+      return skill.skillName == name
+          ? Skill(name, !skill.prof, skill.expert, skill.stat)
+          : skill;
     }).toList();
   }
 }
