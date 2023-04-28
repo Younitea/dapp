eeb933669efbb115b20d28b2048c8425bdc51bc5
Aidan <aidansray@gmail.com>
2023-04-28 14:03:09 -0700

    just so i can pull changes again

--- project/lib/aidans_widgets/deathsave_roller.dart
+++ project/lib/aidans_widgets/deathsave_roller.dart
@@ -8,10 +8,45 @@ class DeathSaveRoller extends ConsumerStatefulWidget {
   ConsumerState<DeathSaveRoller> createState() => _DeathSaveState();
 }
 
+final deathCircle = Container(
+  width: 40,
+  height: 40,
+  decoration: BoxDecoration(
+    //   color: Colors.red,
+    shape: BoxShape.circle,
+    border: Border.all(
+      color: Colors.red,
+      width: 2,
+    ),
+  ),
+);
+
+final lifeCircle = Container(
+  width: 40,
+  height: 40,
+  decoration: BoxDecoration(
+    //   color: Colors.green,
+    shape: BoxShape.circle,
+    border: Border.all(
+      color: Colors.green,
+      width: 2,
+    ),
+  ),
+);
+
 class _DeathSaveState extends ConsumerState<DeathSaveRoller> {
   int _numSuccess = 0;
   int _numFails = 0;
 
   @override
-  Widget build(BuildContext context) => const Text(":)");
+  Widget build(BuildContext context) =>
+      Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+        deathCircle,
+        deathCircle,
+        deathCircle,
+        ElevatedButton(onPressed: () => null, child: const Text("Death Save")),
+        lifeCircle,
+        lifeCircle,
+        lifeCircle
+      ]);
 }
