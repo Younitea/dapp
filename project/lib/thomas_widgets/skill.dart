2dc61d3e20a6a315ca6672355beb7a5e180b6763
Younitea <thomasrchiesa@gmail.com>
2023-04-28 14:38:45 -0700

    uhu

--- project/lib/thomas_widgets/skill.dart
+++ project/lib/thomas_widgets/skill.dart
@@ -3,4 +3,17 @@ class Skill {
   int bonus = 0;
   int stat = 0;
   Skill(this.skillName, this.bonus, this.stat);
+
+  Skill.fromJson(Map<String, dynamic> json)
+      : skillName = json['n'],
+        bonus = json['b'],
+        stat = json['s'];
+
+  Map<String, dynamic> toJson() {
+    return {
+      'n': skillName,
+      'b': stat,
+      's': stat,
+    };
+  }
 }
